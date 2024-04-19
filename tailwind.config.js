30890a3498d3ef29a6610d7ea282b210a0f80e6e
qmvills <queenmary.com@gmail.com>
2024-04-19 16:59:16 +0800

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm:"480px",
+      md:"768px",
+      lg:"1024px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
